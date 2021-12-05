989ce4f6fccee345120e4c08b4b080a85ddb4bda
noernova <noernova666@gmail.com>
2021-12-06 02:35:46 +0700

    add footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,12 +8,14 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
 import NavBar from "../components/navigation/Nav";
+import Footer from "../components/footer/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <NavBar />
       <Component {...pageProps} />
+      <Footer />
     </ThemeProvider>
   );
 }
